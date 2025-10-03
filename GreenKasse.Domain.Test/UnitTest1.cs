297f3ba34104fd2f21f6f9670ca38ab04dd8d7cc
Flavio Reber <reber.flavio@gmail.com>
2025-10-03 16:44:49 +0200

    basic structure with test & cake

---
+++ GreenKasse.Domain.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace GreenKasse.Domain.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        var myPrice = new MyPrice();
+        // Act
+        myPrice.SetPrice(3);
+        // Assert
+        Assert.Equal(3, myPrice.Price);
+    }
+}
---
+++ GreenKasse.Domain/Class1.cs
@@ -0,0 +1,48 @@
+﻿namespace GreenKasse.Domain;
+
+public class MyPrice
+{
+    private int _price = 0;
+
+    public int Price
+    {
+        get
+        {
+            return _price;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _price = value;
+            }
+        }
+    }
+
+    public void MyVoidFunction(string myString)
+    {
+        // ...
+        Console.WriteLine(myString);
+    }
+
+    public MyPrice SetPrice(int price)
+    {
+        _price = price;
+        return this;
+    }
+
+}
+
+public class Foo
+{
+    public Foo()
+    {
+        var myPrice = new MyPrice();
+
+        Console.WriteLine(myPrice
+            .SetPrice(3)
+            .SetPrice(5)
+            .Price
+        );
+    }
+}
