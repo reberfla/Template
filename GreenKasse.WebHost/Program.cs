d45c8e68da34578e60655349a294a421fb756019
Flavio Reber <reber.flavio@gmail.com>
2025-10-03 17:18:47 +0200

    add swagger docs

--- GreenKasse.WebHost/Program.cs
+++ GreenKasse.WebHost/Program.cs
@@ -1,8 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
+
+app.UseOpenApi();
+app.UseSwaggerUi();
+app.UseRouting();
+app.MapControllers();
 
 app.MapGet("/", () => "Hello World!");
 
