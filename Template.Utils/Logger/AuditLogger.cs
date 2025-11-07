722bc4fd0294ed5887c3418b095e5a304019c029
Flavio Reber <reber.flavio@gmail.com>
2025-11-07 13:18:40 +0100

    let format test fail

--- Template.Utils/Logger/AuditLogger.cs
+++ Template.Utils/Logger/AuditLogger.cs
@@ -3,10 +3,10 @@ namespace Template.Utils.Logger;
 public abstract class AuditLogger
 {
     protected AuditType _auditType;
-    private readonly ConsoleLogger _logger = new();
+private readonly ConsoleLogger _logger = new();
 
     protected void WriteAuditLog(string description)
-    {
+{
         AuditEntry message = new(_auditType, description);
         _logger.HandleMessage(message);
     }
